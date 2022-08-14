2d2ad52c6525efd540c4b6b01c28e0e00298e60b
Alejandro Guerrero <alejo.jesus.magne@gmail.com>
2022-08-14 17:35:05 -0400

    pagination nest

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { PokemonModule } from '../pokemon/pokemon.module';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
+import { CommonModule } from '../common/common.module';
 
 @Module({
   controllers: [SeedController],
   providers: [SeedService],
   imports: [
-    PokemonModule
+    PokemonModule,
+    CommonModule
   ]
 })
 export class SeedModule {}
